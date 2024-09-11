e9723385af0393b2315f78768c550faf1bb3a13f
alexeybedrinsky <bedrinsky@mail.ru>
2024-09-11 13:35:38 +0300

    домашка 20_1 переделана под задание 20_2

--- config/settings.py
+++ config/settings.py
@@ -120,7 +120,7 @@ STATICFILES_DIRS = (BASE_DIR / 'static',)
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = ('media/')
+MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from products.apps import ProductsConfig
 
+from products.views import products_list, products_detail
+
 app_name = ProductsConfig.name
 
 urlpatterns = [
-
+    path('', products_list, name='products_list'),
+    path('products/<int:pk>/', products_detail, name='products_detail')
 ]
